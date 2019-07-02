675162c85ced44a0c0cf1d6ade224647ee038d67
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-03 04:52:33 +0700

    Add homepage class

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NotFoundPage from "../components/NotFoundPage";
 import Header from "../components/Header";
+import HomePage from "../components/HomePage";
 
 const AppRouter = () => (
 	<BrowserRouter>
 		<div>
 			<Header />
 			<Switch>
+				<Route path="/" component={HomePage} exact={true} />
 				<Route component={NotFoundPage} />
 			</Switch>
 		</div>
